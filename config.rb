31d73da15d1561590410b3fae8472dd1365487b7
Earth <virudson@gmail.com>
2015-10-20 20:10:22 +0700

    add localization support

--- config.rb
+++ config.rb
@@ -28,7 +28,8 @@ end
 # proxy "/this-page-has-no-template.html", "/template-file.html", :locals => {
 #  :which_fake_page => "Rendering a fake page with a local variable" }
 
-page "/jobs.html", :layout => "jobs_layout"
+page '/jobs.html', layout: 'jobs_layout'
+page '/th/jobs.html', layout: 'jobs_layout'
 
 
 ###
@@ -93,14 +94,14 @@ configure :build do
     f.template_dir  = File.join(root, 'source/images')
     f.output_dir    = File.join(root, 'build/images')
     f.icons = {
-        "_favicon_template.png" => [
-            { icon: "apple-touch-icon-152x152-precomposed.png" },
-            { icon: "apple-touch-icon-144x144-precomposed.png" },
-            { icon: "apple-touch-icon-114x114-precomposed.png" },
-            { icon: "apple-touch-icon-72x72-precomposed.png" },
-            { icon: "apple-touch-icon-precomposed.png", size: "57x57" },
-            { icon: "favicon.png", size: "16x16" },
-            { icon: "favicon.ico", size: "64x64,32x32,24x24,16x16" }
+        '_favicon_template.png' => [
+            { icon: 'apple-touch-icon-152x152-precomposed.png' },
+            { icon: 'apple-touch-icon-144x144-precomposed.png' },
+            { icon: 'apple-touch-icon-114x114-precomposed.png' },
+            { icon: 'apple-touch-icon-72x72-precomposed.png' },
+            { icon: 'apple-touch-icon-precomposed.png', size: '57x57' },
+            { icon: 'favicon.png', size: '16x16' },
+            { icon: 'favicon.ico', size: '64x64,32x32,24x24,16x16' }
         ]
     }
   end
@@ -110,4 +111,4 @@ configure :development do
   activate :livereload
 end
 
-activate :directory_indexes
+activate :i18n#, templates_dir: 'layouts'
